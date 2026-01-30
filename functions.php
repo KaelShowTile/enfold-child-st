01efabe42a81d64c74049a41d0c1d16d73180eee
KaelShowTile <kaelshowtile@gmail.com>
2026-01-30 17:01:46 +1100

    2026013006

--- functions.php
+++ functions.php
@@ -85,6 +85,7 @@ function load_more_projects() {
 // }
 
 //IMPORT PRODUCT
+/*
 add_action('init', function() {
     if (!isset($_GET['run_tile_multi_import'])) return;
 
@@ -365,6 +366,7 @@ add_action('init', function() {
     exit;
 });
 
+*/
 
 // Enqueue all collection scripts
 add_action( 'wp_enqueue_scripts', 'enqueue_all_collection_scripts' );
