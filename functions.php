778c688599afeee7e00138536558afb75ad17a09
KaelShowTile <kaelshowtile@gmail.com>
2026-02-13 09:54:54 +1100

    2026021302

--- functions.php
+++ functions.php
@@ -85,6 +85,7 @@ function load_more_projects() {
 // }
 
 //IMPORT PRODUCT
+/*
 add_action('init', function() {
     if (!isset($_GET['run_tile_multi_import'])) return;
 
@@ -198,6 +199,7 @@ add_action('init', function() {
     exit;
 });
 
+*/
 
 //import project
 /*
