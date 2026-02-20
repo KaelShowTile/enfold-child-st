ecd0da28e7da9596c337b24d988201aa2e44a9b1
KaelShowTile <kaelshowtile@gmail.com>
2026-02-20 13:08:50 +1100

    2026022005

--- assets/js/archive-collection-category.js
+++ assets/js/archive-collection-category.js
@@ -1,18 +1,26 @@
-$('.collection-inner-slider-thumb').each(function() {
-    var thumbEl = this;
-    var previewEl = $(this).siblings('.collection-inner-slider-preview')[0];
-    if (previewEl) {
-        var swiperThumb = new Swiper(thumbEl, {
-            spaceBetween: 10,
-            slidesPerView: 4,
-            freeMode: true,
-            watchSlidesProgress: true,
-        });
-        var swiperPreview = new Swiper(previewEl, {
-            spaceBetween: 10,
-            thumbs: {
-                swiper: swiperThumb,
-            },
+jQuery(document).ready(function($){
+
+    initializePage();
+
+    function initializePage() {
+        $('.collection-inner-slider-thumb').each(function() {
+            var thumbEl = this;
+            var previewEl = $(this).siblings('.collection-inner-slider-preview')[0];
+            if (previewEl) {
+                var swiperThumb = new Swiper(thumbEl, {
+                    spaceBetween: 10,
+                    slidesPerView: 4,
+                    freeMode: true,
+                    watchSlidesProgress: true,
+                });
+                var swiperPreview = new Swiper(previewEl, {
+                    spaceBetween: 10,
+                    thumbs: {
+                        swiper: swiperThumb,
+                    },
+                });
+            }
         });
     }
-});
+})
+
